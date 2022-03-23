e62ac3471ad9ec090ff3d365cbc45a0da9d19113
software-hhao <hhao.software@hotmail.com>
2022-03-23 10:39:04 +0800

    --amend
    1.0.1

---
+++ hhao-extension-spring-boot-starter/src/main/java/com/hhao/common/extension/register/ExtensionBootstrap.java
@@ -0,0 +1,52 @@
+
+/*
+ * Copyright 2018-2022 WangSheng.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hhao.common.extension.register;
+
+import com.hhao.common.extension.annotation.Extension;
+import com.hhao.common.extension.model.ExtensionPoint;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * @author Wang
+ * @since 2022/3/14 14:35
+ */
+public class ExtensionBootstrap implements ApplicationContextAware {
+    @Resource
+    private ExtensionRegister extensionRegister;
+
+    private ApplicationContext applicationContext;
+
+    @PostConstruct
+    public void init(){
+        Map<String, Object> extensionBeans = applicationContext.getBeansWithAnnotation(Extension.class);
+        extensionBeans.values().forEach(
+                extension -> extensionRegister.doRegistration((ExtensionPoint) extension)
+        );
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
